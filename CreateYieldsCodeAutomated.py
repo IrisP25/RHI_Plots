7ab7bc57868f8c540aa0fba071840ed563e44f1d
Iris Ponce <irisponce@Iriss-Mac-mini.local>
2023-06-12 23:32:43 -0400

    First Working Version of the automated Yield comparison to Fist Model

--- CreateYieldsCodeAutomated.py
+++ CreateYieldsCodeAutomated.py
@@ -41,10 +41,10 @@ chi_strange = (strangeFileContent[0].split(","))[-1].strip()
 temp_light =  (lightFileContent[1].split(","))[2].strip()
 chi_light = (lightFileContent[0].split(","))[-1].strip()
 
-temp_all = (allFileContent[0].split(","))[-1].strip()
-chi_all = (allFileContent[1].split(","))[2].strip()
-print (temp_strange,temp_light)
-print (chi_strange,chi_light)
+temp_all = (allFileContent[1].split(","))[2].strip()
+chi_all = (allFileContent[0].split(","))[-1].strip()
+print (temp_strange,temp_light,temp_all)
+print (chi_strange,chi_light,chi_all)
 
 #Now I want to get the particle yields for each files and start formatting them as they will be outputted
 #on the code that will be ran
@@ -83,6 +83,18 @@ for index in range(9,len(allFileContent)-1):
     oneTempDev += ","
     allFileContent.pop()
     
+    twoTempYield += (strangeFileContent[-1].split(","))[6].strip()
+    twoTempYield += ","
+    twoTempYield += (lightFileContent[index].split(","))[6].strip()
+    twoTempYield += ","
+    
+    twoTempDev += (strangeFileContent[-1].split(","))[6].strip()
+    twoTempDev += ","
+    twoTempDev += (lightFileContent[index].split(","))[6].strip()
+    twoTempDev += ","
+    
+    strangeFileContent.pop()
+    lightFileContent.pop()
     #Now the most complicated is the 2 temperature since piKp are from ome 
     continue
   if index == len(allFileContent)-1:
@@ -94,6 +106,10 @@ for index in range(9,len(allFileContent)-1):
     oneTempYield += "};"
     oneTempDev += (allFileContent[index].split(","))[8].strip()
     oneTempDev += "};"
+    twoTempYield += (strangeFileContent[index].split(","))[6].strip()
+    twoTempYield += "};"
+    twoTempDev += (strangeFileContent[index].split(","))[8].strip()
+    twoTempDev += "};"
     
     print (" LAST ENTRY IN FILE DONE")
     continue
@@ -105,13 +121,69 @@ for index in range(9,len(allFileContent)-1):
   oneTempYield += ","
   oneTempDev += (allFileContent[index].split(","))[8].strip()
   oneTempDev += ","
- 
-print (experimentalYields)
+  
+  ###
+  if index< 13 or index ==14:
+      twoTempYield += (lightFileContent[index].split(","))[6].strip()
+      twoTempYield += ","
+      twoTempDev += (lightFileContent[index].split(","))[8].strip()
+      twoTempDev += ","
+  
+  if index >14:
+      twoTempYield += (strangeFileContent[index].split(","))[6].strip()
+      twoTempYield += ","
+      twoTempDev += (strangeFileContent[index].split(","))[8].strip()
+      twoTempDev += ","
+
 print (experimentalYieldErr)
 print (oneTempYield)
 print (oneTempDev)
+print (twoTempYield)
+print (twoTempDev)
 
+#Now we need to read in the Yield code Fernando gave me. We will modify the lines with the yield inputs as
+#but also the temperature and Chi info from the legends!
+yieldPlot = open(args.inputDirectory+"/Yieldsndevs_39.C")
+yieldPlotContent = yieldPlot.readlines()
 
+newFile = "YieldPlot"+args.energyForYield+".C"
+with open(newFile,'w') as outputFile:
+    outputFile.writelines("void YieldPlot"+args.energyForYield+"() \n")
+    for index_out in range(2,len(yieldPlotContent)):
+        ##first replace the yield info!!
+        if index_out <10:
+            outputFile.writelines(yieldPlotContent[index_out])
+        elif index_out ==10:
+            outputFile.writelines(oneTempYield+"\n")
+        elif index_out ==11:
+            outputFile.writelines(twoTempYield+"\n")
+        elif index_out == 13:
+            outputFile.writelines(experimentalYields+"\n")
+        elif index_out == 14:
+            outputFile.writelines(experimentalYieldErr+"\n")
+        elif index_out == 16:
+            outputFile.writelines(oneTempDev+"\n")
+        elif index_out == 17:
+            outputFile.writelines(twoTempDev+"\n")
+        elif index_out == 147:
+            oneTempLegend = yieldPlotContent[147].split()
+            oneTempLegend[(oneTempLegend.index("MeV,")-1)] = temp_all
+            oneTempLegend[(oneTempLegend.index("dof")+2)] = chi_all+'","L");'
+            outputFile.writelines((" ".join(oneTempLegend))+"\n")
+        elif index_out == 148:
+            lightTempLegend = yieldPlotContent[148].split()
+            lightTempLegend[(lightTempLegend.index("MeV,")-1)] = temp_light
+            lightTempLegend[(lightTempLegend.index("dof)_{")+3)] = chi_light+'","L");'
+            outputFile.writelines((" ".join(lightTempLegend))+"\n")
+        elif index_out == 149:
+            strangeTempLegend = yieldPlotContent[149].split()
+            strangeTempLegend[(strangeTempLegend.index("MeV,")-1)] = temp_strange
+            strangeTempLegend[(strangeTempLegend.index("dof)_{")+3)] = chi_strange+'","L");'
+            outputFile.writelines((" ".join(strangeTempLegend))+"\n")
+        else:
+            outputFile.writelines(yieldPlotContent[index_out])
+        
+outputFile.close()
 
 
 
